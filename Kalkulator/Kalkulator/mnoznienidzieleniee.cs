8ed85fa14462e12dbf6b7b8a489a64decc3efc07
karol <Student@DESKTOP-GGOJP1D>
2020-02-25 13:51:55 +0100

    + klasa mnozenie dzielnie

---
+++ Kalkulator/Kalkulator/mnoznienidzieleniee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class mnoznienidzieleniee
+    {
+        public static double mnozenie(double a, double b)
+        {
+            return a * b;
+        }
+        public static double dzielenie(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
