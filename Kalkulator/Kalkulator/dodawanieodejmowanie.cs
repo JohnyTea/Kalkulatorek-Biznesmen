d36d3ff806992566326e61e38ee993b771a46fe9
karol <Student@DESKTOP-GGOJP1D>
2020-02-25 13:56:42 +0100

    eluwina kalda dodaj minus

---
+++ Kalkulator/Kalkulator/dodawanieodejmowanie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class dodawanieodejmowanie
+    {
+        public static double dodawanie(double a, double b)
+        {
+            return a + b;
+        }
+        public static double odejmowanie(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
